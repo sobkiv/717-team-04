af36b174596549ed0a7c0914cca4d889fb8d5ba0
andrewromanchuk1 <95419027+andrewromanchuk1@users.noreply.github.com>
2022-10-27 21:22:35 +0300

    added carousel in kind-words section

--- js/kind-carousel.js
+++ js/kind-carousel.js
@@ -11,11 +11,31 @@
    function renderSlide() {
       const slideContainer = document.querySelector('.kind-carousel-container');
       slideContainer.innerHTML = slides[currentSlideIndx];
+      if (window.innerWidth > 768) {
+         const secondSlideInd = currentSlideIndx + 1 >= slides.length ? 0 : currentSlideIndx + 1;
+         slideContainer.innerHTML += slides[secondSlideInd];
+         if (window.innerWidth > 990) {
+            const thirdSlideInd = secondSlideInd + 1 >= slides.length ? 0 : secondSlideInd + 1;
+            const fourthSlideInd = thirdSlideInd + 1 >= slides.length ? 0 : thirdSlideInd + 1;
+         slideContainer.innerHTML += slides[thirdSlideInd] +  slides[fourthSlideInd];
+         }
+      }
    }
    function nextSlide() {
       currentSlideIndx = currentSlideIndx + 1 >= slides.length ? 0 : currentSlideIndx + 1;
       renderSlide();
    }
+   function prevSlide() {
+      currentSlideIndx = currentSlideIndx - 1 < 0 ? slides.length - 1 : currentSlideIndx - 1;
+      renderSlide();
+   }
    setInterval(nextSlide, 3000);
    renderSlide();
+   const nextBtn = document.querySelector('.kind-carousel-next');
+   nextBtn.addEventListener('click', nextSlide);
+
+   const prevBtn = document.querySelector('.kind-carousel-prev');
+   prevBtn.addEventListener('click', prevSlide);
+
+   window.addEventListener('resize', renderSlide)
 })();
