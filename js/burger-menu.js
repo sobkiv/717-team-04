9f28d5994b9dd3b7e62b0af0e3be9f8050c92baa
sobkiv <pavlo.sobkiv@gmail.com>
2022-11-08 01:28:47 +0200

    Made semantical names

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,18 +1,18 @@
 (function () {
-    const iconBurger = document.querySelector(".header__burger-menu");
+    const burgerButton = document.querySelector(".header__burger-menu");
     const navigationsBody = document.querySelector(".header__menu-conteiner");
-    iconBurger.addEventListener("click", function (e) {
+    burgerButton.addEventListener("click", function (e) {
       document.body.classList.toggle("lock");
-      iconBurger.classList.toggle("active");
+      burgerButton.classList.toggle("active");
       navigationsBody.classList.toggle("active");
     });
   
     const navLinks = document.querySelectorAll(".header__menu-item-hover");
     navLinks.forEach((navLink) => {
       navLink.addEventListener("click", (_) => {
-            if (iconBurger.classList.contains("active")) {
+            if (burgerButton.classList.contains("active")) {
                 document.body.classList.remove("lock");
-                iconBurger.classList.remove("active");
+                burgerButton.classList.remove("active");
                 navigationsBody.classList.remove("active");
             }
           }
